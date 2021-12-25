f0d4336a69f675a3cfe38d5467e18d6458f7bd1d
Kamol Ibragimov <k.ibragimov@softteco.com>
2021-12-25 17:56:40 +0000

    Updated project for Blazor WebAssembly

--- GreenOnion/Program.cs
+++ GreenOnion/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace GreenOnion.Client
+namespace GreenOnion
 {
     public class Program
     {
