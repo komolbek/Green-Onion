b7413f3c0dcaeca261247435d9319e97e145b734
Kamol Ibragimov <k.ibragimov@softteco.com>
2022-01-03 15:32:11 +0000

    implemented ProjectManager class's method parameters

--- Services/ProjectManagerService.cs
+++ Services/ProjectManagerService.cs
@@ -15,58 +15,64 @@ namespace GreenOnion.Services
             this.projectDataMapper = projectDataMapper;
         }
 
-        public Project CreateProject()
+        public Project CreateProject(string name, string companyID, string creatorID, DateTime? dueDate, Ticket[]? tickets)
         {
 
             return new Project();
         }
 
-        public bool DeleteProject()
+        // generates current date to add to startDate attribute of Project while creating object.
+        private DateTime generateStartDate()
+        {
+            return new DateTime();
+        }
+
+        public bool DeleteProject(string projID)
         {
             return true;
         }
 
-        public Project GetProject()
+        public Project GetProject(string projID)
         {
             return new Project();
         }
 
-        public bool ChangeProject()
+        public bool ChangeProject(string projID, string? name, DateTime? dueDate, Ticket[]? tickets)
         {
             return true;
         }
 
-        public bool AddMember()
+        public bool AddMember(string userID, string projID)
         {
             return true;
         }
 
-        public User[] GetMembers()
+        public User[] GetMembers(string projID)
         {
             return new User[]{};
         }
 
-        public Ticket[] GetTickets()
+        public Ticket[] GetTickets(string projID)
         {
             return new Ticket[]{};
         }
 
-        public bool AddTicket()
+        public bool AddTicket(string projID, string ticketID)
         {
             return true;
         }
 
-        public bool RemoveTicket()
+        public bool RemoveTicket(string projID, string ticketID)
         {
             return true;
         }
 
-        public bool MoveTicketTo()
+        public bool MoveTicket(string projID, string ticketID, string toList)
         {
             return true;
         }
 
-        public Dictionary<string, Ticket[]> GetTicketsSortedInProjectLists()
+        public Dictionary<string, Ticket[]> GetTicketsSortedInProjectLists(string projID)
         {
             return new Dictionary<string, Ticket[]>();
         }
