27cf7d3971a2ee6ae59995e55ca53d658e245b22
Kamol Ibragimov <k.ibragimov@softteco.com>
2022-01-03 19:56:51 +0000

    created mock methods of PredictionManager class

--- Services/PredictionManagerService.cs
+++ Services/PredictionManagerService.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using GreenOnion.DomainModels;
+using System.Collections.Generic;
+
 namespace GreenOnion.Services
 {
     public class PredictionManagerService
     {
+        private Prediction prediction;
+        private List<Project> projects;
+
         public PredictionManagerService()
         {
+        }
+
+        public int calculateRequiredNumOfTickets()
+        {
+            return 0;
+        }
+
+        public int calculateRequiredNumOfMembers()
+        {
+            return 0;
+        }
+
+        public Prediction MakePrediction()
+        {
+            return new Prediction();
+        }
+
+        // 
+        public string calculateProjectDurationByTicketComplexity()
+        {
+            return "";
+        }
+
+        public string calculateProjectDurationByHistoricalData()
+        {
+            return "";
+        }
+
+        private string makeStringFromDate()
+        {
+            return "";
         }
     }
 }
