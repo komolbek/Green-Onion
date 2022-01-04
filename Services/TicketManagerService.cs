a8160eaab56ee725f1884e5279d2def7f7b66065
Kamol Ibragimov <k.ibragimov@softteco.com>
2022-01-04 15:31:14 +0000

    created mock methods & attribute of TicketManager class

--- Services/TicketManagerService.cs
+++ Services/TicketManagerService.cs
@@ -1,10 +1,46 @@
 ﻿using System;
+using GreenOnion.DataMappers;
+using GreenOnion.DomainModels;
+
 namespace GreenOnion.Services
 {
     public class TicketManagerService
     {
+        private TicketDataMapper ticketDataMapper;
+
         public TicketManagerService()
         {
+            this.ticketDataMapper = new TicketDataMapper();
+        }
+
+        public bool CreateTicket()
+        {
+            return true;
+        }
+
+        public bool ChangeTicket()
+        {
+            return true;
+        }
+
+        public bool DeleteTicket()
+        {
+            return true;
+        }
+
+        public Ticket GetTicket()
+        {
+            return new Ticket();
+        }
+
+        public bool AssignMember()
+        {
+            return true;
+        }
+
+        public bool RemoveMember()
+        {
+            return true;
         }
     }
 }
