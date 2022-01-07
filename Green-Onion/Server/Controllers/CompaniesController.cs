e6352cff49c8c7b54fbe303f4297adb2bd1306d0
Kamol Ibragimov <rusmirolub@gmail.com>
2022-01-07 14:30:54 +0000

    [server] implemented company controller, context classes

--- Green-Onion/Server/Controllers/CompaniesController.cs
+++ Green-Onion/Server/Controllers/CompaniesController.cs
@@ -1,10 +1,125 @@
-﻿using System;
+﻿using GreenOnion.DomainModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using GreenOnion.Server.DataLayer.DataAccess;
+using System.Linq;
+using System.Collections.Generic;
+
 namespace GreenOnion.Server.Controllers
 {
-    public class CompaniesController
+    [Route("api/CompaniesController")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
     {
-        public CompaniesController()
+
+        private readonly CompanyDbContext _context;
+
+        public CompaniesController(CompanyDbContext context)
         {
+            this._context = context;
+        }
+
+        // Create company
+        // POST: api/Company
+        [HttpPost]
+        public async Task<ActionResult<Company>> CreateCompany(Company company)
+        {
+            _context.companies.Add(company);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCompanyById), new { id = company.CompanyID }, company);
+        }
+
+        // Add project into the context
+        // PUT: api/Company
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Company>> AddProject(string companyID, Project project)
+        {
+            Company copmpany = await _context.companies.FindAsync(companyID);
+            copmpany.Projects.Add(project);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(companyID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // Get projects of company by its id
+        // GET: api/Company
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Project>>> GetProjects(string companyID)
+        {
+            Company company = await _context.companies.FindAsync(companyID);
+
+            return company.Projects;
+        }
+
+        // Delete company by id
+        // DELETE: api/Company
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCompany(string companyID)
+        {
+            Company company = await _context.companies.FindAsync(companyID);
+            _context.companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // Change company data by its id
+        // DELETE: api/Company
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> ChangeCompany(string companyID, Company newCompany)
+        {
+            if (companyID != newCompany.CompanyID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(newCompany).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(companyID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // Get company by id
+        // GET: api/Company
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompanyById(string companyID) => await _context.companies.FindAsync(companyID);
+
+
+        private bool CompanyExists(string id)
+        {
+            return _context.companies.Any(e => e.CompanyID == id);
         }
     }
 }
---
+++ Green-Onion/Server/DataLayer/DataAccess/CompanyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GreenOnion.DomainModels;
+
+namespace GreenOnion.Server.DataLayer.DataAccess
+{
+    public class CompanyDbContext : DbContext
+    {
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Company> companies { get; set; }
+    }
+}
